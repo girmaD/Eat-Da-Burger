07e8876501887373adea288ec6142dbb63e4fb33
Girma Ebssa <girma.ebssa@gmail.com>
2020-12-03 14:35:51 -0800

    set up my server.js file

--- server.js
+++ server.js
@@ -1,4 +1,31 @@
-const handlebars = require('express-handlebars');
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+const routes = require("./controlllers/burgers_controllers.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
 
 
 
