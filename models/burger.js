83308c3ce1f1c20e218117c88f7c5a8bf4beee3d
Girma Ebssa <girma.ebssa@gmail.com>
2020-12-04 10:28:56 -0800

    set up all the model functions comming from orm.js

--- models/burger.js
+++ models/burger.js
@@ -5,6 +5,17 @@ const burgers = {
         orm.selectAll('burgers', (res) => {
             cb(res)
         });
-    }
+    },
+    inserOne: function(colName, colValue, cb) {
+        orm.insertOne('burgers', colName, colValue, (res) => {
+            cb(res)
+        });
+    },
+    updateOne: function(colName, colValue, condition, cb) {
+        orm.updateOne('burgers', colName, colValue, condition, (res) => {
+            cb(res)
+        });
+    }       
 }
+// Export the database functions for the controller (burgers_controller.js).
 module.exports = burgers;
