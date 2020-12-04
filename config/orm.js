cea9a7f19dcc4161cc71138eede0e2197dcc6797
Girma Ebssa <girma.ebssa@gmail.com>
2020-12-04 10:27:31 -0800

    set up all the three functions related to manipulating the database

--- config/orm.js
+++ config/orm.js
@@ -1,19 +1,47 @@
 const connection = require('./connection.js');
 
+
 const orm = {
     selectAll: function(tableName, cb) {
-        let queryString = `Select * from ??`;
-        connection.query(queryString, [tableName], (err, data) =>{
+        let queryString = "Select * from" + tableName + ";"
+        connection.query(queryString, (err, data) => {
             if(err) throw err;
             cb(data)
         })
     },
-    insertOne: function(tableName, colName, colValue) {
-        
+    //INSERT INTO TABALE (COLNAME) VALUES (COLVALUE)
+    insertOne: function(tableName, colName, colValue, cb) {
+        let query = "INSERT INTO " + tableName;
+        query += " (";
+        query += colName;
+        query += ")";
+        query += " VALUES (";
+        query += "?";
+        query += ")";
+
+        connection.query(query, colValue, function(err, result) {
+            if (err) {
+              throw err;
+            }      
+            cb(result);
+        });        
     },
-    updateOne: function(tableName, colName, colValue, conditionColName, conditionValue) {
+    //UPDATE TABLE SET COLNAME= COLVALUE WHERE CONDITION
+    updateOne: function(tableName, colName, colValue, condition, cb) {
+       const query = "UPDATE " + tableName;
+       query += " SET ";
+       query += colName;
+       query += " = ";
+       query += colValue;
+       query += " WHERE ";
+       query += condition;
        
+       connection.query(query, (err, result) => {
+         if(err) throw err;
+         cb(result)
+       })
     }
 };
 
+// Export the orm object for the model (burger.js).
 module.exports = orm;
