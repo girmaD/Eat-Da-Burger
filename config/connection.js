0e106b8acd7f28e19bc4dbc7b2ce27c0547ee087
Girma Ebssa <girma.ebssa@gmail.com>
2020-12-08 22:10:17 -0800

    change connection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql");
-require("dotenv").config();
+require("dotenv").config(); 
 
+let connection;
 
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL)
@@ -13,21 +14,8 @@ if(process.env.JAWSDB_URL){
     database: "burgers_db",
   });
 }
-// const connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: process.env.MYSQL_PASS,
-//     database: "burgers_db",
-// });
+
 
 connection.connect();
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
 
 module.exports = connection;
